7e34a3f59a64ebd25b16d1d1b7f90add819f6511
Jessica <jessica.homet@gmail.com>
2019-07-29 11:53:24 -0700

    reset head

--- bikes/python.py
+++ bikes/python.py
@@ -1,11 +1,6 @@
 data = {}
 
-
 for bike in data["bikes"]:
      f= open("%s.json" % bike, "w+")
      f.write(str(data["bikes"][bike]).replace("'", '"'))
      f.close()
-
-
-# for i in range(10):
-#     f.write("This is line %d\r\n" % (i+1))
